2a9802c59039eebec23b9a0eb8d8729d9df6f5eb
yassu <yassu0320.dev@gmail.com>
2018-05-19 20:00:08 +0900

    :heavy_plus_sign: Add parser and table-name option

--- sql_by_csv/main.py
+++ sql_by_csv/main.py
@@ -3,6 +3,7 @@
 
 import unittest
 import csv
+from optparse import OptionParser
 import sys,os
 sys.path.append(os.path.expanduser('./../sql_by_csv/'))
 from sql_by_csv import insert_sql, elements
@@ -12,20 +13,32 @@ def get_row_elements(row, types):
         _type = elements.isit(types[i])
         yield _type(item)
 
+def get_parser():
+    parser = OptionParser()
+    parser.add_option(
+        '--table-name', '-t',
+        action='store',
+        dest='table_name',
+        type='string',
+        help='table name'
+    )
+    return parser
+
 def main():
+    opts, args = get_parser().parse_args()
+
+    # TODO: table_nameがNoneだったらError
     filename = 'example_insert.csv'
 
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         cols = next(reader)
-        print('cols: {}'.format(cols))
         types = next(reader)
-        print('types: {}'.format(types))
         table_name = 'TableName'    # TODO: コマンドラインの引数から取るようにする
 
         for row in reader:
             print(
-                str(insert_sql.InsertQuery(table_name,
+                str(insert_sql.InsertQuery(opts.table_name,
                     cols,
                     list(get_row_elements(row, types))
                 ))
